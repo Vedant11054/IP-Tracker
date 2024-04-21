28eda7a78b0c5761fb2e8252031cc4b8f0693bee
Vedant11054 <164635371+Vedant11054@users.noreply.github.com>
2024-04-21 19:03:10 +0530

    app.py

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/track-ip', methods=['POST'])
-def track_ip():
-    ip_address = request.form['ip']
-    access_token = 'your_token_here'
-    url = f'https://ipinfo.io/{ip_address}?token={access_token}'
-    response = requests.get(url)
-    if response.status_code == 200;
-        data = response.json()
+def get_ip_info(ip_address):
+    url = f"https://ipinfo.io/{ip_address}/json"
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
             city = data.get('city', 'Unknown')
             region = data.get('region', 'Unknown')
             country = data.get('country', 'Unknown')
@@ -25,8 +19,19 @@ def track_ip():
                 'Country': country,
                 'ISP': isp
             }
-    else:
-        return jsonify({"error": "Failed to retrieve information"}), response.status_code
+        else:
+            return None
+    except requests.exceptions.RequestException:
+        return None
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    ip_info = None
+    if request.method == 'POST':
+        ip_address = request.form['ip_address']
+        if ip_address:
+            ip_info = get_ip_info(ip_address)
+    return render_template('index.html', ip_info=ip_info)
 
 if __name__ == '__main__':
     app.run(debug=True)
